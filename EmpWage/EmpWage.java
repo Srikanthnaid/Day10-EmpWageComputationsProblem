e603405586b316298bf30a4053c190ba0d9b601e
Srikanthnaid <ksru044gmail.com>
2022-11-14 14:35:07 +0530

    Commite Emp Wage of Multiple Comapnies using interface

---
+++ EmpWage/EmpWage.java
@@ -0,0 +1,6 @@
+package EmpWage;
+
+public interface EmpWage {
+	void wagesForWorkingHourAndDays();
+    void addEmpWage(String company, int WAGE_PER_HR, int MAX_WORKING_DAYS, int MAX_HRS_IN_MONTH);
+}
